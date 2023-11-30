0d1d968a033ac1ee21b4ff227af5be6ba6cfc3c0
SANDIP CHITALE <sandipchitale@gmail.com>
2023-11-30 08:14:19 -0800

    Try with IP. Delegate to DefaultHostnameVerifier.

--- src/main/java/sandipchitale/trustloopback/TrustloopbackApplication.java
+++ src/main/java/sandipchitale/trustloopback/TrustloopbackApplication.java
@@ -4,6 +4,7 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.DefaultHostnameVerifier;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -37,9 +38,15 @@ public class TrustloopbackApplication {
 	}
 
 	private static class LoopbackIPHostnameVerifier implements HostnameVerifier {
+
+		private final DefaultHostnameVerifier defaultHostnameVerifier = new DefaultHostnameVerifier();
+
 		@Override
 		public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
-			return "127.0.0.1".equals(hostname);
+			if ("127.0.0.1".equals(hostname)) {
+				return true;
+			}
+			return defaultHostnameVerifier.verify(hostname, sslSession);
 		}
 	}
 
@@ -74,39 +81,46 @@ public class TrustloopbackApplication {
 			}
 
 			try {
-                System.out.println("Trying to access https://127.0.0.1:8080/ with SslBundle 'client' expecting failure");
+				System.out.println("Trying to access https://127.0.0.1:8080/ with SslBundle 'client' expecting failure");
 				System.out.println(restTemplate.getForObject("https://127.0.0.1:8080/", String.class));
 			} catch (RestClientException restClientException) {
 				System.out.println("Expected exception: " + restClientException.getMessage());
 			}
 
 			restTemplate = restTemplateBuilder.requestFactory(() -> {
-                // Build request factory with SSLContext from SslBundle but with a custom LoopbackIPHostnameVerifier
-                SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(
-                        clientSslBundle
-                                .getManagers()
-                                .createSslContext(clientSslBundle.getProtocol()),
-                        new LoopbackIPHostnameVerifier());
+				// Build request factory with SSLContext from SslBundle but with a custom LoopbackIPHostnameVerifier
+				SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(
+						clientSslBundle
+								.getManagers()
+								.createSslContext(clientSslBundle.getProtocol()),
+						new LoopbackIPHostnameVerifier());
 
-                HttpClientConnectionManager httpClientConnectionManager = PoolingHttpClientConnectionManagerBuilder
-                        .create()
-                        .setSSLSocketFactory(sslConnectionSocketFactory)
-                        .build();
-                CloseableHttpClient closeableHttpClient = HttpClients.custom()
-                                                                     .setConnectionManager(httpClientConnectionManager)
-                                                                     .evictExpiredConnections()
-                                                                     .build();
-                HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(closeableHttpClient);
+				HttpClientConnectionManager httpClientConnectionManager = PoolingHttpClientConnectionManagerBuilder
+						.create()
+						.setSSLSocketFactory(sslConnectionSocketFactory)
+						.build();
+				CloseableHttpClient closeableHttpClient = HttpClients.custom()
+																	 .setConnectionManager(httpClientConnectionManager)
+																	 .evictExpiredConnections()
+																	 .build();
+				HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(closeableHttpClient);
 				return requestFactory;
 			}).build();
 
-            try {
-                System.out.println("Trying to access https://127.0.0.1:8080/ with SslBundle 'client' but with a custom LoopbackIPHostnameVerifier expecting success");
-                System.out.println(restTemplate.getForObject("https://127.0.0.1:8080/", String.class));
+			try {
+				System.out.println("Trying to access https://10.0.0.194:8080/ with SslBundle 'client' but with a custom LoopbackIPHostnameVerifier expecting failure");
+				System.out.println(restTemplate.getForObject("https://10.0.0.194:8080/", String.class));
+				System.out.println("Unexpected success");
+			} catch (RestClientException e) {
+				System.out.println("Expected Exception:" + e.getMessage());
+			}
+			try {
+				System.out.println("Trying to access https://127.0.0.1:8080/ with SslBundle 'client' but with a custom LoopbackIPHostnameVerifier expecting success");
+				System.out.println(restTemplate.getForObject("https://127.0.0.1:8080/", String.class));
 				System.out.println("Success");
-            } catch (RestClientException e) {
-                System.out.println("Unexpected Exception:" + e.getMessage());
-            }
+			} catch (RestClientException e) {
+				System.out.println("Unexpected Exception:" + e.getMessage());
+			}
 		};
 	}
 }
